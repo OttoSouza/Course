e9682abd25c5ca415cf8f6e379bee764c00aade8
OttoSouza <ottosdeveloper@gmail.com>
2019-10-30 23:53:23 -0300

    More Things

---
+++ knowledge/arrays.js
@@ -0,0 +1,13 @@
+const MyArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+console.log('New ' + MyArray.map(el => el ** 2));
+console.log('Origin ' + MyArray);
+
+// filter();
+
+// reduce();
+
+// find();
+
+// include();
